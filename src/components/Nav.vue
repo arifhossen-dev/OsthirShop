64f7e701262c4ec67f2fab09668e9211556cd23c
arifhossen-dev <ahak.bsl@gmail.com>
2023-09-22 00:11:49 +0600

    updare

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -13,46 +13,13 @@
         </div>
         <div class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
           <div class="flex items-center flex-shrink-0">
-            <img class="w-auto h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+            OsthirShop
           </div>
           <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
-            <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-            <a href="#" class="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900 border-b-2 border-indigo-500">Dashboard</a>
-            <a href="#" class="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700">Team</a>
-            <a href="#" class="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700">Projects</a>
-            <a href="#" class="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700">Calendar</a>
+            <RouterLink to="/" class="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900 border-b-2 border-indigo-500">Home</RouterLink>
+            <RouterLink to="" class="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700">All Products</RouterLink>
+            
           </div>
-        </div>
-        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <button type="button" class="relative p-1 text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            <span class="absolute -inset-1.5" />
-            <span class="sr-only">View notifications</span>
-            <BellIcon class="w-6 h-6" aria-hidden="true" />
-          </button>
-
-          <!-- Profile dropdown -->
-          <Menu as="div" class="relative ml-3">
-            <div>
-              <MenuButton class="relative flex text-sm bg-white rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                <span class="absolute -inset-1.5" />
-                <span class="sr-only">Open user menu</span>
-                <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-              </MenuButton>
-            </div>
-            <transition enter-active-class="transition duration-200 ease-out" enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-in" leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
-              <MenuItems class="absolute right-0 z-10 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <MenuItem v-slot="{ active }">
-                  <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Profile</a>
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</a>
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign out</a>
-                </MenuItem>
-              </MenuItems>
-            </transition>
-          </Menu>
         </div>
       </div>
     </div>
@@ -60,10 +27,8 @@
     <DisclosurePanel class="sm:hidden">
       <div class="pt-2 pb-4 space-y-1">
         <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
-        <DisclosureButton as="a" href="#" class="block py-2 pl-3 pr-4 text-base font-medium text-indigo-700 border-l-4 border-indigo-500 bg-indigo-50">Dashboard</DisclosureButton>
-        <DisclosureButton as="a" href="#" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Team</DisclosureButton>
-        <DisclosureButton as="a" href="#" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Projects</DisclosureButton>
-        <DisclosureButton as="a" href="#" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Calendar</DisclosureButton>
+        <DisclosureButton as="a" href="/" class="block py-2 pl-3 pr-4 text-base font-medium text-indigo-700 border-l-4 border-indigo-500 bg-indigo-50">Home</DisclosureButton>
+        <DisclosureButton as="a" href="/" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">All Products</DisclosureButton>
       </div>
     </DisclosurePanel>
   </Disclosure>
--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive, onBeforeMount } from 'vue'
+import axios from 'axios'
 import {
   Disclosure,
   DisclosureButton,
@@ -15,46 +16,60 @@ import {
 } from '@headlessui/vue'
 import { StarIcon } from '@heroicons/vue/20/solid'
 import { HeartIcon, MinusIcon, PlusIcon } from '@heroicons/vue/24/outline'
+import { useRoute } from 'vue-router'
+const route = useRoute()
 
-const product = {
-  name: 'Zip Tote Basket',
-  price: '$140',
-  rating: 4,
-  images: [
-    {
-      id: 1,
-      name: 'Angled view',
-      src: 'https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg',
-      alt: 'Angled front view with bag zipped and handles upright.',
-    },
-    // More images...
-  ],
-  colors: [
-    { name: 'Washed Black', bgColor: 'bg-gray-700', selectedColor: 'ring-gray-700' },
-    { name: 'White', bgColor: 'bg-white', selectedColor: 'ring-gray-400' },
-    { name: 'Washed Gray', bgColor: 'bg-gray-500', selectedColor: 'ring-gray-500' },
-  ],
-  description: `
-    <p>The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.</p>
-  `,
-  details: [
-    {
-      name: 'Features',
-      items: [
-        'Multiple strap configurations',
-        'Spacious interior with top zip',
-        'Leather handle and tabs',
-        'Interior dividers',
-        'Stainless strap loops',
-        'Double stitched construction',
-        'Water-resistant',
-      ],
-    },
-    // More sections...
-  ],
-}
+const product = reactive({});
+onBeforeMount(() => {
+  axios.get(`https://dummyjson.com/products/${route.params.id}`)
+    .then(function (response) {
+      console.log(response.data);
+      Object.assign(product, response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+})
 
-const selectedColor = ref(product.colors[0])
+// const product = {
+//   name: 'Zip Tote Basket',
+//   price: '$140',
+//   rating: 4,
+//   images: [
+//     {
+//       id: 1,
+//       name: 'Angled view',
+//       src: 'https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg',
+//       alt: 'Angled front view with bag zipped and handles upright.',
+//     },
+//     // More images...
+//   ],
+//   colors: [
+//     { name: 'Washed Black', bgColor: 'bg-gray-700', selectedColor: 'ring-gray-700' },
+//     { name: 'White', bgColor: 'bg-white', selectedColor: 'ring-gray-400' },
+//     { name: 'Washed Gray', bgColor: 'bg-gray-500', selectedColor: 'ring-gray-500' },
+//   ],
+//   description: `
+//     <p>The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.</p>
+//   `,
+//   details: [
+//     {
+//       name: 'Features',
+//       items: [
+//         'Multiple strap configurations',
+//         'Spacious interior with top zip',
+//         'Leather handle and tabs',
+//         'Interior dividers',
+//         'Stainless strap loops',
+//         'Double stitched construction',
+//         'Water-resistant',
+//       ],
+//     },
+//     // More sections...
+//   ],
+// }
+
+const selectedColor = ref(null)
 </script>
 <template>
   <div class="bg-white">
@@ -65,30 +80,33 @@ const selectedColor = ref(product.colors[0])
           <!-- Image selector -->
           <div class="hidden w-full max-w-2xl mx-auto mt-6 sm:block lg:max-w-none">
             <TabList class="grid grid-cols-4 gap-6">
-              <Tab v-for="image in product.images" :key="image.id" class="relative flex items-center justify-center h-24 text-sm font-medium text-gray-900 uppercase bg-white rounded-md cursor-pointer hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4" v-slot="{ selected }">
-                <span class="sr-only">{{ image.name }}</span>
+              <Tab v-for="image in product.images" :key="image.id"
+                class="relative flex items-center justify-center h-24 text-sm font-medium text-gray-900 uppercase bg-white rounded-md cursor-pointer hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
+                v-slot="{ selected }">
                 <span class="absolute inset-0 overflow-hidden rounded-md">
-                  <img :src="image.src" alt="" class="object-cover object-center w-full h-full" />
+                  <img :src="image" alt="" class="object-cover object-center w-full h-full" />
                 </span>
-                <span :class="[selected ? 'ring-indigo-500' : 'ring-transparent', 'pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2']" aria-hidden="true" />
+                <span
+                  :class="[selected ? 'ring-indigo-500' : 'ring-transparent', 'pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2']"
+                  aria-hidden="true" />
               </Tab>
             </TabList>
           </div>
 
           <TabPanels class="w-full aspect-h-1 aspect-w-1">
             <TabPanel v-for="image in product.images" :key="image.id">
-              <img :src="image.src" :alt="image.alt" class="object-cover object-center w-full h-full sm:rounded-lg" />
+              <img :src="image" :alt="image.alt" class="object-cover object-center w-full h-full sm:rounded-lg" />
             </TabPanel>
           </TabPanels>
         </TabGroup>
 
         <!-- Product info -->
         <div class="px-4 mt-10 sm:mt-16 sm:px-0 lg:mt-0">
-          <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ product.name }}</h1>
+          <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ product.title }}</h1>
 
           <div class="mt-3">
             <h2 class="sr-only">Product information</h2>
-            <p class="text-3xl tracking-tight text-gray-900">{{ product.price }}</p>
+            <p class="text-3xl tracking-tight text-gray-900">${{ product.price }}</p>
           </div>
 
           <!-- Reviews -->
@@ -96,7 +114,9 @@ const selectedColor = ref(product.colors[0])
             <h3 class="sr-only">Reviews</h3>
             <div class="flex items-center">
               <div class="flex items-center">
-                <StarIcon v-for="rating in [0, 1, 2, 3, 4]" :key="rating" :class="[product.rating > rating ? 'text-indigo-500' : 'text-gray-300', 'h-5 w-5 flex-shrink-0']" aria-hidden="true" />
+                <StarIcon v-for="rating in [0, 1, 2, 3, 4]" :key="rating"
+                  :class="[product.rating > rating ? 'text-indigo-500' : 'text-gray-300', 'h-5 w-5 flex-shrink-0']"
+                  aria-hidden="true" />
               </div>
               <p class="sr-only">{{ product.rating }} out of 5 stars</p>
             </div>
@@ -111,25 +131,16 @@ const selectedColor = ref(product.colors[0])
           <form class="mt-6">
             <!-- Colors -->
             <div>
-              <h3 class="text-sm text-gray-600">Color</h3>
-
-              <RadioGroup v-model="selectedColor" class="mt-2">
-                <RadioGroupLabel class="sr-only">Choose a color</RadioGroupLabel>
-                <span class="flex items-center space-x-3">
-                  <RadioGroupOption as="template" v-for="color in product.colors" :key="color.name" :value="color" v-slot="{ active, checked }">
-                    <div :class="[color.selectedColor, active && checked ? 'ring ring-offset-1' : '', !active && checked ? 'ring-2' : '', 'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none']">
-                      <RadioGroupLabel as="span" class="sr-only">{{ color.name }}</RadioGroupLabel>
-                      <span aria-hidden="true" :class="[color.bgColor, 'h-8 w-8 rounded-full border border-black border-opacity-10']" />
-                    </div>
-                  </RadioGroupOption>
-                </span>
-              </RadioGroup>
+              <h3 class="text-sm text-gray-600">{{ product.stock }} available</h3>
             </div>
 
             <div class="flex mt-10">
-              <button type="submit" class="flex items-center justify-center flex-1 max-w-xs px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full">Add to bag</button>
+              <button type="submit"
+                class="flex items-center justify-center flex-1 max-w-xs px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full">Add
+                to bag</button>
 
-              <button type="button" class="flex items-center justify-center px-3 py-3 ml-4 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500">
+              <button type="button"
+                class="flex items-center justify-center px-3 py-3 ml-4 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500">
                 <HeartIcon class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
                 <span class="sr-only">Add to favorites</span>
               </button>
@@ -143,10 +154,13 @@ const selectedColor = ref(product.colors[0])
               <Disclosure as="div" v-for="detail in product.details" :key="detail.name" v-slot="{ open }">
                 <h3>
                   <DisclosureButton class="relative flex items-center justify-between w-full py-6 text-left group">
-                    <span :class="[open ? 'text-indigo-600' : 'text-gray-900', 'text-sm font-medium']">{{ detail.name }}</span>
+                    <span :class="[open ? 'text-indigo-600' : 'text-gray-900', 'text-sm font-medium']">{{ detail.name
+                    }}</span>
                     <span class="flex items-center ml-6">
-                      <PlusIcon v-if="!open" class="block w-6 h-6 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
-                      <MinusIcon v-else class="block w-6 h-6 text-indigo-400 group-hover:text-indigo-500" aria-hidden="true" />
+                      <PlusIcon v-if="!open" class="block w-6 h-6 text-gray-400 group-hover:text-gray-500"
+                        aria-hidden="true" />
+                      <MinusIcon v-else class="block w-6 h-6 text-indigo-400 group-hover:text-indigo-500"
+                        aria-hidden="true" />
                     </span>
                   </DisclosureButton>
                 </h3>
